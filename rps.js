8223adfff98d0c9e7fbeb179e8da86abf08dffeb
Eddi Perz F <eddi@flowneuroscience.com>
2025-08-07 12:07:47 +0200

    Finished basic game

--- rps.js
+++ rps.js
@@ -25,43 +25,67 @@ function playRound(humanChoice, compChoice){
                 break;
             case "paper":
                 humanScore += 1;
-                console.log("Human wins!");
+                console.log("Human wins this round!");
+                break;
             case "scissors":
                 compScore +=1;
-                console.log("Computer wins!")
+                console.log("Computer wins this round!")
+                break;
         }
+        return;
     }
     else if(comp=="paper"){
          switch (human){
             case "rock":
                 compScore += 1;
-                console.log("Computer wins!");
+                console.log("Computer wins this round!");
                 break;
             case "paper":
                 console.log("It's a draw!");
+                break;
             case "scissors":
                 humanScore += 1;
-                console.log("Human wins!")
-
+                console.log("Human wins this round!")
+                break;
         }
+        return;
     }
     else if(comp=="scissors"){
         switch (human){
             case "rock":
                 humanScore += 1;
-                console.log("Human wins!");
+                console.log("Human wins this round!");
                 break;
             case "paper":
                 compScore += 1;
-                console.log("Computer wins!");
+                console.log("Computer wins this round!");
+                break;
             case "scissors":
                 console.log("It's a draw!")
-
-
+                break;
         }
+        return;
     }
+    return;
 }
 
-let testC= getComputerChoice();
-let testH = getHumanChoice();
-playRound(testH, testC);
+function playGame(){
+    for(let i=0; i<5; i++){
+        compChoice = getComputerChoice();
+        humanChoice = getHumanChoice();
+        playRound(humanChoice, compChoice);
+        console.log(`Current score: \n Human: ${humanScore} \n Computer: ${compScore}`);
+    }
+    if(humanScore > compScore){
+        console.log(`The winner of the game is human with score ${humanScore} vs ${compScore}!`);
+    }
+    else if(compScore > humanScore){
+        console.log(`The winner of the game is computer with score ${compScore} vs ${humanScore}!`);
+    }
+    else if(compScore == humanScore){
+        console.log("Oh shit, it's a draw!")
+    }
+
+}
+
+playGame();
